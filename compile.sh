2b486c2e5451afb1e8491eada48e353446673137
Space Banana <tomasbessa@protonmail.com>
2023-05-28 22:35:47 +0100

    Binaries are archived

--- compile.sh
+++ compile.sh
@@ -3,24 +3,31 @@ mkdir bin
 
 #Linux
 echo "Compiling for Linux x86_64"
-go build -o bin/parasol-linux parasol.go
+go build -o bin/parasol parasol.go
+zip -v -m -3 parasol-linux.zip bin/parasol
 echo "Compiling for Linux ARM64"
-GOARCH=arm64 go build -o bin/parasol-linux-arm parasol.go
+GOARCH=arm64 go build -o bin/parasol parasol.go
+zip -v -m -3 parasol-linux-arm.zip bin/parasol
 
 #Windows
 echo "Compiling for Windows x86_64"
-GOOS=windows GOARCH=amd64 go build -o bin/parasol-windows.exe parasol.go
+GOOS=windows GOARCH=amd64 go build -o bin/parasol.exe parasol.go
+zip -v -m -3 parasol-windows.zip bin/parasol.exe
 
 #MacOS
 echo "Compiling for MacOS x86_64"
-GOOS=darwin GOARCH=amd64 go build -o bin/parasol-macos parasol.go
+GOOS=darwin GOARCH=amd64 go build -o bin/parasol parasol.go
+zip -v -m -3 parasol-macos.zip bin/parasol
 echo "Compiling for MacOS ARM64"
-GOOS=darwin GOARCH=arm64 go build -o bin/parasol-macos-arm parasol.go
+GOOS=darwin GOARCH=arm64 go build -o bin/parasol parasol.go
+zip -v -m -3 parasol-macos-arm.zip bin/parasol
 
 #FreeBSD
 echo "Compiling for FreeBSD x86_64"
-GOOS=darwin GOARCH=amd64 go build -o bin/parasol-freebsd parasol.go
+GOOS=darwin GOARCH=amd64 go build -o bin/parasol parasol.go
+zip -v -m -3 parasol-freebsd.zip bin/parasol
 
 #OpenBSD
 echo "Compiling for OpenBSD x86_64"
-GOOS=darwin GOARCH=amd64 go build -o bin/parasol-openbsd parasol.go
+GOOS=darwin GOARCH=amd64 go build -o bin/parasol parasol.go
+zip -v -m -3 parasol-openbsd.zip bin/parasol
