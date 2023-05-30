748caf69e2f016d877ac273f1b31f3464f9cbb48
Space Banana <tomasbessa@protonmail.com>
2023-05-30 16:36:02 +0100

    Small shell improvements

--- compile.sh
+++ compile.sh
@@ -1,33 +1,36 @@
-#!/bin/bash
+#!/bash
 mkdir bin
 
 #Linux
 echo "Compiling for Linux x86_64"
-go build -o bin/parasol parasol.go; strip bin/parasol
-zip -v -m -3 parasol-linux.zip bin/parasol
+go build -o parasol parasol.go; strip parasol
+zip -v -m -3 parasol-linux.zip parasol
 echo "Compiling for Linux ARM64"
-GOARCH=arm64 go build -o bin/parasol parasol.go; strip bin/parasol
-zip -v -m -3 parasol-linux-arm.zip bin/parasol
+GOARCH=arm64 go build -o parasol parasol.go; strip parasol
+zip -v -m -3 parasol-linux-arm.zip parasol
 
 #Windows
 echo "Compiling for Windows x86_64"
-GOOS=windows GOARCH=amd64 go build -o bin/parasol.exe parasol.go; strip bin/parasol.exe
-zip -v -m -3 parasol-windows.zip bin/parasol.exe
+GOOS=windows GOARCH=amd64 go build -o parasol.exe parasol.go; strip parasol.exe
+zip -v -m -3 parasol-windows.zip parasol.exe
 
 #MacOS
 echo "Compiling for MacOS x86_64"
-GOOS=darwin GOARCH=amd64 go build -o bin/parasol parasol.go; strip bin/parasol
-zip -v -m -3 parasol-macos.zip bin/parasol
+GOOS=darwin GOARCH=amd64 go build -o parasol parasol.go; strip parasol
+zip -v -m -3 parasol-macos.zip parasol
 echo "Compiling for MacOS ARM64"
-GOOS=darwin GOARCH=arm64 go build -o bin/parasol parasol.go; strip bin/parasol
-zip -v -m -3 parasol-macos-arm.zip bin/parasol
+GOOS=darwin GOARCH=arm64 go build -o parasol parasol.go; strip parasol
+zip -v -m -3 parasol-macos-arm.zip parasol
 
 #FreeBSD
 echo "Compiling for FreeBSD x86_64"
-GOOS=darwin GOARCH=amd64 go build -o bin/parasol parasol.go; strip bin/parasol
-zip -v -m -3 parasol-freebsd.zip bin/parasol
+GOOS=darwin GOARCH=amd64 go build -o parasol parasol.go; strip parasol
+zip -v -m -3 parasol-freebsd.zip parasol
 
 #OpenBSD
 echo "Compiling for OpenBSD x86_64"
-GOOS=darwin GOARCH=amd64 go build -o bin/parasol parasol.go; strip bin/parasol
-zip -v -m -3 parasol-openbsd.zip bin/parasol
+GOOS=darwin GOARCH=amd64 go build -o parasol parasol.go; strip parasol
+zip -v -m -3 parasol-openbsd.zip parasol
+
+
+mv *.zip bin/
