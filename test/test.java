28684076785509c1b7fca9d18d70b6208cc01383
Space Banana <tomasbessa@protonmail.com>
2024-09-04 20:39:03 +0100

    further freebsd tests

--- test/test.java
+++ test/test.java
@@ -5,16 +5,48 @@ import java.nio.file.FileStore;
 
 //import bananatui.*;
 
+//sometimes its better to test functionality in a small isolated program
+//i mostly use this for testing functionality on systems i dont daily drive, like freebsd and windows
+
 public class test {
   public static void main(String[] args) {
-    freebsd_test();
+    freebsd_devicetest();
   }
-  private static void freebsd_test() {
+  private static void freebsd_devicetest() {
+    String[] invalid_paths = new String[]{
+      "", "/dev", "/usr/src", "/usr/ports", "/var/", "/tmp",
+      "/home", "/zroot"
+    };
     for (FileStore store: FileSystems.getDefault().getFileStores()) {
-      var a = store.toString();
-      //if (!a.contains("(/dev")) {continue;}
-      println(a); println(store.type());
+      var path = store.toString();
+      if (invalidPath_softmatch(invalid_paths, path)) {continue;}
+      path = sanitize_path(null);
+      println(path); println("Filesystem: " + store.type());
     }
+  }
+
+  private static boolean invalidPath_softmatch(String[] invalid_paths, String path) {
+    for (String p : invalid_paths) {
+      if (path.contains(p)) {return true;}
+    }
+    return false;
+  }
+
+  private static int getFilterStart(String path) {
+    for (int i = path.length()-1; i >= 0; i--) {
+      if (path.charAt(i) == '(') {return i;}
+    }
+    return path.length();
+  }
+
+  private static String sanitize_path(String path) {
+    String final_path = "";
+    int copy_end = getFilterStart(final_path);
+
+    for (int i = 0; i < copy_end; i++) {
+      final_path += path.charAt(i);
+    }
+    return final_path;
   }
 
   private static void println(String msg) {System.out.println(msg);}
