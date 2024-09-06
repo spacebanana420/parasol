1cd079dcf8a6f5e9fc3fa138157a2c6288f73d98
Space Banana <tomasbessa@protonmail.com>
2024-09-06 13:51:20 +0100

    Shell improvements

--- src/parasolib/shell.java
+++ src/parasolib/shell.java
@@ -10,27 +10,20 @@ public class shell {
   public static void runShell() {
     base.clear();
     base.println(getStartMessage());
-    String lineStart = base.foreground("green") + "~ " + base.foreground("default");
     while (true) {
+      String lineStart =
+        base.foreground("green")
+        + "[" + System.getProperty("user.name") + ":" + shortenPath(browser.browser_directory)
+        + "]$ " + base.foreground("default");
+
       base.print(lineStart);
       String prompt = userinput.readUserInput().trim();
-      if (prompt.equals(":q") || prompt.equals(":quit")) {return;}
+      if (userPromptedExit(prompt)) {return;}
       if (prompt.length() == 0) {continue;}
+
       String[] cmd = misc.groupStrings(prompt);
       if (cmd[0].equals("cd")) {
-        if (cmd.length == 1) {browser.browser_directory = System.getProperty("user.home");}
-        else if (cmd[1].equals("..")) {
-          String newparent = new File(browser.browser_directory).getParent();
-          if (newparent != null) {browser.browser_directory = newparent;}
-        }
-        else if (onlyDots(cmd[1])) {continue;}
-        else if (new File(cmd[1]).isDirectory()) {
-          browser.browser_directory = new File(cmd[1]).getAbsolutePath();
-        }
-        else if (new File(browser.browser_directory + "/" + cmd[1]).isDirectory()) {
-          browser.browser_directory += System.getProperty("path.separator") + cmd[1];
-        }
-        else {base.println("cd error: " + cmd[1] + " is not a directory!");}
+        changeDirectory(cmd);
         continue;
       }
       try {
@@ -60,5 +53,33 @@ public class shell {
       if (path.charAt(i) != '.') {return false;}
     }
     return true;
+  }
+
+  private static boolean userPromptedExit(String prompt) {
+    return prompt.equals(":q") || prompt.equals(":quit") || prompt.equals("exit");
+  }
+
+  private static void changeDirectory(String[] cmd) {
+    if (cmd.length == 1) {browser.browser_directory = System.getProperty("user.home");}
+    else if (cmd[1].equals("..")) {
+      String newparent = new File(browser.browser_directory).getParent();
+      if (newparent != null) {browser.browser_directory = newparent;}
+    }
+    else if (onlyDots(cmd[1])) {return;}
+    else if (new File(cmd[1]).isDirectory()) {
+      browser.browser_directory = new File(cmd[1]).getAbsolutePath();
+    }
+    else if (new File(browser.browser_directory + "/" + cmd[1]).isDirectory()) {
+      browser.browser_directory += System.getProperty("path.separator") + cmd[1];
+    }
+    else {base.println("cd error: " + cmd[1] + " is not a directory!");}
+  }
+
+  private static String shortenPath(String path) {
+    String home = System.getProperty("user.home");
+    if (misc.startsWith(path, home)) {
+      return  "~" + path.replaceFirst(home, "");
+    }
+    return path;
   }
 }
