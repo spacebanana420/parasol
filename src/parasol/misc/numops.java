4994bf1a7e6f62a8aaa1194848a3bc2fa4b5b2ca
Space Banana <tomasbessa@protonmail.com>
2025-02-11 23:56:28 +0000

    String number verification optimisation

--- src/parasol/misc/numops.java
+++ src/parasol/misc/numops.java
@@ -3,22 +3,18 @@ package parasol.misc;
 public class numops {
   public static boolean isUint(String s) {
     if (s.length() == 0 || s == null) {return false;}
-    return parse(s, 0);
+    return verifyDigits(s, 0, (byte)'0', (byte)'9');
   }
   public static boolean isInt(String s) {
     if (s.length() == 0 || s == null) {return false;}
     
     int i = (s.charAt(0) == '-') ? 1 : 0;
-    return parse(s, i);
+    return verifyDigits(s, i, (byte)'0', (byte)'9');
   }
-  private static boolean parse(String s, int i) {
-    char[] digits = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+  private static boolean verifyDigits(String s, int i, byte min_char_value, byte max_char_value) {
     if (i >= s.length()) {return true;}
-    boolean isDigit = false;
-    for (int d = 0; d < digits.length; d++) {
-      if (s.charAt(i) == digits[d]) {isDigit = true; break;}
-    }
-    if (!isDigit) {return false;}
-    return parse(s, i+1);
+    int char_value = (int)s.charAt(i);
+    if (char_value < min_char_value || char_value > max_char_value) {return false;}
+    return verifyDigits(s, i+1, min_char_value, max_char_value);
   }
 }
